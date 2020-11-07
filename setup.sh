a2d3c201bdc23b32a849e7f92b687e14c905e222
none <oimej.mad@gmail.com>
2020-11-06 18:21:43 -0800

    PVC

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
-docker build -t wp-image wordpress/
+#minikube delete
+#minikube --vm-driver=docker start --extra-config=apiserver.service-node-port-range=1-35000
+#eval $(minikube docker-env)
+kubectl delete --all service
+kubectl delete --all deployments
+sh setup-metallb.sh
+docker build -t wp-image --build-arg MINIKUBEIP=$(minikube ip) wordpress/
 docker build -t mysql-image mysql/
 docker build -t pma-image phpmyadmin/
-docker build -t nginx-image nginx/
+docker build -t nginx-image --build-arg MINIKUBEIP=$(minikube ip) nginx/
 docker build -t ftp-image ftp/
 docker build -t influxdb-image influxdb/
 docker build -t grafana-image grafana/
+kubectl create -f pv-pvc/
 kubectl create -f .
 #docker run -p 8086:8086 -d -it --net mine --name influxdb myinfluxdb
 #docker run -p 80:5050 -it -d --net mine --name wordpress mywordpress
